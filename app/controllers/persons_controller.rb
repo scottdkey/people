35d320d7970a6eb7abc58a249f553d89491d6785
Scott Key <scottdkey@gmail.com>
2020-02-19 17:22:49 -0700

    rails is correcting person to people in routing. big problem

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -5,8 +5,40 @@ class PersonsController < ApplicationController
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 
   def new
+    @person = Person.new
+    render partial: "form"
   end
+
+  def edit
+    @person = Person.find(params[:id])
+    render partial: "form"
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to persons_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @person = Person.find(params[:id]).destroy
+    redirect_to persons_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :age, :hair_color, :eye_color, :gender, :alive)
+  end
+
+
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ApplicationRecord
+
+  def full_name
+    # Person.find(params[:first_name]) + " " + Person.find(params[:last_name])
+    self.first_name + " " + self.last_name
+  end
 end
