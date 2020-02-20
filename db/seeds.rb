fec9c6f3fbbbed8db8d9af7861853ab9db196f52
Scott Key <scottdkey@gmail.com>
2020-02-19 22:00:27 -0700

    Faker is generating fake data. Ready for turn in.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,16 @@
 
 # For the purpose of creating data later - this is the format.
 
-
-Friend.create(first_name:"Steve", last_name:"Stevenson", age: 31, hair_color:"Blond", eye_color: "Blue", gender: "male", birth: 19880324, living: true)
+10.times do
+  Friend.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.number(digits: 2),
+    hair_color: Faker::Color.color_name,
+    eye_color: Faker::Color.color_name,
+    gender: Faker::Gender.type,
+    birth: Faker::Date.birthday(min_age:18, max_age:85),
+    living: Faker::Boolean.boolean,
+  )
+end
+# Friend.create(first_name:"Steve", last_name:"Stevenson", age: 31, hair_color:"Blond", eye_color: "Blue", gender: "male", birth: 19880324, living: true)
